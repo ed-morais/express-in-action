70dee77eed1e7db53174c1f683a46f7803335f2f
Edilson Morais <moraisbritoedilson@gmail.com>
2022-11-06 16:31:36 -0300

    creating chapter_3 folder and helloexpress.js file

---
+++ chapter_3/helloexpress.js
@@ -0,0 +1,10 @@
+var express = require("express")
+var http = require("http")
+var app = express()
+
+app.use(function(request, response) {
+    console.log("In comes a request to" + request.url)
+    response.end("Hello, Express! (:")
+})
+
+http.createServer(app).listen(3000)
