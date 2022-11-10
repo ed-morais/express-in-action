609065a079094b2c51a86a2c47115b0a2588cf50
Edilson Morais <moraisbritoedilson@gmail.com>
2022-11-10 14:41:25 -0300

    updating app.js

--- chapter_3/static-file-fun/app.js
+++ chapter_3/static-file-fun/app.js
@@ -1,15 +1,31 @@
-var express = require("express")
-var logger = require("morgan")
-var fs = require("fs")
+var express = require("express");
+var logger = require("morgan");
+var fs = require("fs");
 
-var app = express()
+var app = express();
 
-app.use(function(req, res, next) {
-    console.log("Request IP: " + req.url)
-    console.log("Request Date: " + new Date())
-    next()
-})
+app.use(function (req, res, next) {
+  console.log("Request IP: " + req.url);
+  console.log("Request Date: " + new Date());
+  next();
+});
 
-app.listen(3000, function() {
-    console.log("App started on port 3000")
-})
+app.use(function (req, res, next) {
+  var filePath = path.join(__dirname, "static", req.url);
+  fs.stat(filePath, function (err, fileInfo) {
+    if (err) {
+      next();
+      return;
+    }
+
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+      next();
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("App started on port 3000");
+});
