b2b92d89794a2e2ad57e69877398cd39eaebd83e
Edilson Morais <moraisbritoedilson@gmail.com>
2022-11-10 15:07:22 -0300

    writing middleware to app.js

--- chapter_3/static-file-express/app.js
+++ chapter_3/static-file-express/app.js
@@ -1,31 +1,19 @@
 var express = require("express");
-var logger = require("morgan")
-var path = require("path")
+var logger = require("morgan");
+var path = require("path");
 
 var app = express();
 
-app.use(logger("short"))
+app.use(logger("short"));
 
-app.use(function (req, res, next) {
-  var filePath = path.join(__dirname, "static", req.url);
-  fs.stat(filePath, function(err, fileInfo) {
-    if (err) {
-      next();
-      return;
-    }
-    if (fileInfo.isFile()) {
-      res.sendFile(filePath);
-    } else {
-      next();
-    }
-  });
+var staticPath = path.join(__dirname, "static");
+app.use(express.static(staticPath));
+
+app.use(function (req, res) {
+  res.status(404);
+  res.send("File not found");
 });
 
-app.use(function(req, res){
-    res.status(404)
-    res.send("File not found")
-})
-
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("App started on port 3000");
 });
